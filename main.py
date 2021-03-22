5aa89f4be1f3328afded2e6e17e698c581bdc1a3
sm0862512 <Sm0862512@otc.edu>
2021-03-22 17:59:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+Number1 = 0
+Number2 = 0
+Operators = 0
+
+Number1 = int(input("number1. "))
+
+Operator = int(input("operator. "))
+
+Number2 = int(input("number2 "))
+
+if Number1 >= 10:
+  print("Invalid Number.")
